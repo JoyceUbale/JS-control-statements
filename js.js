08c478a4c1565fe61207a8796b88821b500dfc07
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2023-10-24 22:22:15 +0530

    done

---
+++ js.js
@@ -0,0 +1,26 @@
+// Challenge 1: Create a for loop to print all the even numbers till 100 in the console.
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+  // Challenge 2: Create a while loop to print all the odd numbers till 100 in the console.
+  var n = 0;
+  while (n < 100) {
+    n++;
+    if (n % 2 !== 0) {
+      console.log(n);
+    }
+  }
+  // Challenge 3: Create a do while loop to print all the multiples of 3 till 100 in the console.
+  var a=0;
+  do{
+    a++;
+    if(a%3==0){
+      console.log(a);
+    }
+  }while(a<100);
+  // Challenge 4: Create a for loop to print all the prime numbers till 100 in the console.
+  for(let i=2;i<=100;i++){
+      console.log(i);
+  }
